48d95c6e0fc3a014e42025fdd6e63ccd764c32f0
bilel-kedidi <bilel.kedidi@gmail.com>
2016-08-16 20:48:44 +0100

    fix enumeration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def delete_link(url, options={})
+    options = {
+        :method => :delete,
+        :data => {:confirm => t(:text_are_you_sure)}
+    }.merge(options)
+
+    link_to "<i class='fa fa-lg fa-trash-o'></i>".html_safe, url, options
+  end
 end
