19b2dc2fbdb142053c7fffb6859d09a5f275a3ae
bilel-kedidi <bilel.kedidi@gmail.com>
2016-08-16 22:40:15 +0100

    fix I18n

---
+++ app/models/religion_type.rb
@@ -0,0 +1,21 @@
+class ReligionType < Enumeration
+  has_many :core_demographics
+
+  OptionName = :enumeration_religion_type
+
+  def option_name
+    OptionName
+  end
+
+  def objects
+    CoreDemographic.where(:religion_id => self.id)
+  end
+
+  def objects_count
+    objects.count
+  end
+
+  def transfer_relations(to)
+    objects.update_all(:activity_id => to.id)
+  end
+end
