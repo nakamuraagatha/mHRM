5ececd1fb6d779ad5cfc7c3922e3b2ec83268682
bilel.kedidi <bilel@classnco.com>
2016-09-09 16:17:57 +0100

    add validation

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,8 @@
 class Attachment < ApplicationRecord
   mount_uploader :file, AttachmentUploader
 
+  validates_presence_of :file, :type, :owner_id
+
   def self.safe_attributes
     [:id, :file, :type, :description, :owner_id, :_destroy]
   end
