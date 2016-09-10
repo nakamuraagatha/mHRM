bb504a70425c96d5f78e8840cfeae50251a316f2
bilel-kedidi <bilel.kedidi@gmail.com>
2016-09-10 15:54:58 +0100

    fix bugs

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,7 +1,7 @@
 class Language < ApplicationRecord
   belongs_to :user
   belongs_to :language_type
-  belongs_to :proficient_type, foreign_key: :proficient_id
+  belongs_to :proficiency_type, foreign_key: :proficiency_id
 
   has_many :language_attachments, foreign_key: :owner_id
   accepts_nested_attributes_for :language_attachments, reject_if: :all_blank, allow_destroy: true
@@ -11,7 +11,7 @@ class Language < ApplicationRecord
   end
 
   def self.safe_attributes
-    [:user_id, :note, :language_type_id, :proficient_id,
+    [:user_id, :note, :language_type_id, :proficiency_id,
      language_attachments_attributes: [Attachment.safe_attributes]]
   end
 
