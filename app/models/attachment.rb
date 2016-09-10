3163e7db13db3b431fe93286e14d73f593ee54e8
bilel-kedidi <bilel.kedidi@gmail.com>
2016-09-10 17:07:42 +0100

    fixing attachment bug

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,7 @@
 class Attachment < ApplicationRecord
   mount_uploader :file, AttachmentUploader
 
-  validates_presence_of :file, :type, :owner_id
+  validates_presence_of :file, :type
 
   def self.safe_attributes
     [:id, :file, :type, :description, :owner_id, :_destroy]
