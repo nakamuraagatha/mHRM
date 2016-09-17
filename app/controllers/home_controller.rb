d398bb4c31a95cfcc3d205aae9549cf868182049
bilel-kedidi <bilel.kedidi@gmail.com>
2016-09-17 17:40:20 +0100

    change workflow of the btns

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
   def index
+    if session[:employee_id]
+      session[:employee_id] = nil
+      flash[:notice]= "Logged Off from #{@employee.login}"
+      redirect_to root_path
+    end
     @setting = Setting.first || Setting.new
   end
 end
