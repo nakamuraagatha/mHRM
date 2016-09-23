8ba9c71de7effb475ae6374a75ed5c4283d15c23
bilel-kedidi <bilel.kedidi@gmail.com>
2016-09-23 14:00:51 +0100

    fix bug when loggin out

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     if session[:employee_id]
+      @employee = User.find session[:employee_id]
       session[:employee_id] = nil
       flash[:notice]= "Logged Off from #{@employee.login}"
       redirect_to root_path
