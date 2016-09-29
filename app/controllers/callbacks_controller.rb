cbbe6552153dfb844c89eee669bedc59c3333326
bilel-kedidi <bilel.kedidi@gmail.com>
2016-09-29 20:10:54 +0100

    testing office 365 login

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -5,7 +5,10 @@ class CallbacksController < Devise::OmniauthCallbacksController
   end
 
   def office365
+    logger.warn 'OFFICE check'
+    session["omniauth.state"] = params['state']
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    logger.warn 'OFFICE CREATE OR UPDATE USER'
     sign_in_and_redirect @user
   end
 end
